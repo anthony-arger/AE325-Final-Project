ce90c6ab0cbd2456064f7f9abf760db4676e7bc0
Gabriel Stavrakas <gabstav@umich.edu>
2025-04-18 02:25:20 -0400

    Cruise speed calc function added

---
+++ MatLab/CruiseSpeed.m
@@ -0,0 +1,13 @@
+function [cruise_spd] = CruiseSpeed(CL,CLmax, S, rho, W)
+%CRUISESPEED Summary of this function goes here
+%   Detailed explanation goes here
+% S: planform area
+% W: weight of the plane
+Vstall = sqrt((2*W)/(rho*S*CLmax));
+cruise_spd = sqrt((2*W)/(rho*S*CL));
+if cruise_spd >= Vstall
+    display('ALERT: AIRCRAFT IS STALLING AT THIS CRUISING SPEED')
+    display('REDESIGN WING')
+end
+end
+
