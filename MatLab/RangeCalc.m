ce06320d9537c9ca01070a41277583f4a65daf8b
anthony-arger <anthony.arger@pm.me>
2025-04-17 22:32:04 -0400

    Pre Aircraft Change

--- MatLab/RangeCalc.m
+++ MatLab/RangeCalc.m
@@ -1,5 +1,7 @@
-function [range] = RangeCalc(W,Ps,conditions)
-%at cruise
-C = 0.46 %lbs/hp-h
- Range = (conditions.U_inf*W.Wi/(Ps*4*C))*log(W.Wi/W.Wf)
+function [Range] = RangeCalc(W,Ps,conditions)
+    %at cruise
+    C = 0.46; %lbs/hp-h
+    conditions.U_inf = conditions.U_inf .* 2.237;
+    Range = (conditions.U_inf * W.Wi / (Ps*4*C) ) * log(W.Wi/W.Wf);
+
 end
